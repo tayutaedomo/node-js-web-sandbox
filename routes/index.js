52bd145e55bf8fc92fd4120f0d58c83a261a84fe
tayutaedomo <tayutaedomo@gmail.com>
2016-06-28 19:53:20 +0900

    Add guide_setup trials

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,18 @@ router.get('/quick_start_without_auto', function(req, res, next) {
   res.render('quick_start_without_auto', { title: 'Quick Start without auto setup' });
 });
 
+router.get('/guide_setup', function(req, res, next) {
+  res.render('guide_setup', { title: 'Guide: Setup' });
+});
+
+router.get('/guide_setup_centered', function(req, res, next) {
+  res.render('guide_setup_centered', { title: 'Guide: Setup with play button centered' });
+});
+
+router.get('/guide_setup_dynamically', function(req, res, next) {
+  res.render('guide_setup_dynamically', { title: 'Guide: Setup with dynamically loaded' });
+});
+
 
 module.exports = router;
 
