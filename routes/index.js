a7050a1d408319fac6996ea41c8cf99ec9594a71
tayutaedomo <tayutaedomo@gmail.com>
2016-06-28 17:53:49 +0900

    Add quick_start trials

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Video.js Trial' });
 });
 
+router.get('/quick_start', function(req, res, next) {
+  res.render('quick_start', { title: 'Quick Start' });
+});
+
+router.get('/quick_start_without_auto', function(req, res, next) {
+  res.render('quick_start_without_auto', { title: 'Quick Start without auto setup' });
+});
+
+
 module.exports = router;
 
