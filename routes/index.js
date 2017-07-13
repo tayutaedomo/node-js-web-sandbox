c45670dee6298cc6a6702a263c01f2f373f5d17c
tayutaedomo <tayutaedomo@gmail.com>
2017-07-13 22:09:40 +0900

    Refactoring of path

--- routes/index.js
+++ routes/index.js
@@ -12,36 +12,36 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Video.js Trial' });
 });
 
-router.get('/quick_start', function(req, res, next) {
-  res.render('quick_start', { title: 'Quick Start' });
+router.get('/videojs/quick_start', function(req, res, next) {
+  res.render('videojs/quick_start', { title: 'Quick Start' });
 });
 
-router.get('/quick_start_without_auto', function(req, res, next) {
-  res.render('quick_start_without_auto', { title: 'Quick Start without auto setup' });
+router.get('/videojs/quick_start_without_auto', function(req, res, next) {
+  res.render('videojs/quick_start_without_auto', { title: 'Quick Start without auto setup' });
 });
 
-router.get('/guide_setup', function(req, res, next) {
-  res.render('guide_setup', { title: 'Guide: Setup' });
+router.get('/videojs/guide_setup', function(req, res, next) {
+  res.render('videojs/guide_setup', { title: 'Guide: Setup' });
 });
 
-router.get('/guide_setup_centered', function(req, res, next) {
-  res.render('guide_setup_centered', { title: 'Guide: Setup with play button centered' });
+router.get('/videojs/guide_setup_centered', function(req, res, next) {
+  res.render('videojs/guide_setup_centered', { title: 'Guide: Setup with play button centered' });
 });
 
-router.get('/guide_setup_dynamically', function(req, res, next) {
-  res.render('guide_setup_dynamically', { title: 'Guide: Setup with dynamically loaded' });
+router.get('/videojs/guide_setup_dynamically', function(req, res, next) {
+  res.render('videojs/guide_setup_dynamically', { title: 'Guide: Setup with dynamically loaded' });
 });
 
-router.get('/contrib_hls', function(req, res, next) {
-  res.render('contrib_hls', { title: 'videojs-contrib-hls' });
+router.get('/videojs/contrib_hls', function(req, res, next) {
+  res.render('videojs/contrib_hls', { title: 'videojs-contrib-hls' });
 });
 
-router.get('/design_resizing', function(req, res, next) {
-  res.render('design_resizing', { title: 'Design: Resizing' });
+router.get('/videojs/design_resizing', function(req, res, next) {
+  res.render('videojs/design_resizing', { title: 'Design: Resizing' });
 });
 
-router.get('/events', function(req, res, next) {
-  res.render('events', { title: 'Events' });
+router.get('/videojs/events', function(req, res, next) {
+  res.render('videojs/events', { title: 'Events' });
 });
 
 
