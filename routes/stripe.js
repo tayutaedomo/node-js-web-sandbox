ee1f40d70d33c70c0abbe1eaf17c7909675c710a
tayutaedomo <tayutaedomo@gmail.com>
2017-11-04 17:44:43 +0900

    Add stripe/checkout_price

--- routes/stripe.js
+++ routes/stripe.js
@@ -60,6 +60,60 @@ router.post('/checkout', function(req, res) {
   });
 });
 
+router.get('/checkout_price', function(req, res) {
+  res.render('stripe/checkout_price', {
+    title : 'Stripe Checkout without Price',
+    data: { params: {} }
+  });
+});
+
+router.post('/checkout_price', function(req, res) {
+  var payload = {
+    title : 'Stripe Checkout without Price',
+    data: {
+      params: {},
+      result_list: []
+    }
+  };
+
+  payload.data.params.email = req.body.email;
+  payload.data.params.token = req.body.stripeToken;
+  debug(payload.data.params);
+
+  stripe.customers.create({
+    email:  payload.data.params.email,
+    source: payload.data.params.token
+
+  }).then(function(customer) {
+    // YOUR CODE: Save the customer ID and other info in a database for later.
+
+    debug(customer);
+    payload.data.result_list.push(customer);
+
+    return stripe.charges.create({
+      amount: 2000,
+      currency: "usd",
+      customer: customer.id
+    });
+
+  }).then(function(charge) {
+    // Use and save the charge info.
+
+    debug(charge);
+    payload.data.result_list.push(charge);
+    payload.data.result = JSON.stringify(payload.data.result_list, null, 2);
+
+    res.render('stripe/checkout_price', payload);
+
+  }).catch(function(err) {
+    console.error(err.stack);
+
+    payload.data.error = err;
+
+    res.render('stripe/checkout_price', payload);
+  });
+});
+
 router.get('/charge', function(req, res) {
   res.render('stripe/charge', {
     title : 'Stripe Charge',
