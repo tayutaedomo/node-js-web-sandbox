3c8a40cc0ed738eec882ae94ae5ad402c126c565
tayutaedomo <tayutaedomo@gmail.com>
2018-12-29 19:54:09 +0900

    Add file_upload/multiparty

--- routes/file_upload.js
+++ routes/file_upload.js
@@ -6,13 +6,16 @@ const multer = require('multer');
 const upload_local = multer({ dest: os.tmpdir() });
 const upload_memoney = multer({}); // No file output, file.buffer only
 
+const util = require('util')
+const multiparty = require('multiparty');
+
 
 router.post('/multer_local', upload_local.fields([ { name: 'file' } ]), function(req, res) {
   console.log('files', req.files);
   console.log('files.file[0]', req.files.file[0]);
   console.log('body', req.body);
 
-  var payload =  {
+  const payload =  {
     title : 'Local Store | Multer',
     data: {}
   };
@@ -24,7 +27,7 @@ router.post('/multer_memory', upload_memoney.fields([ { name: 'file' } ]), funct
   console.log('files', req.files);
   console.log('body', req.body);
 
-  var payload =  {
+  const payload =  {
     title : 'On Memory | Multer',
     data: {}
   };
@@ -33,9 +36,30 @@ router.post('/multer_memory', upload_memoney.fields([ { name: 'file' } ]), funct
 });
 
 
+router.post('/multiparty', function(req, res) {
+  const payload =  {
+    title : 'multiparty',
+    data: {}
+  };
+
+  const form = new multiparty.Form();
+
+  form.parse(req, function(err, fields, files) {
+    if (err) console.error(err.stack || err);
+
+    console.log('body', req.body);
+    console.log('fields', fields);
+    console.log('files', util.inspect(files, { colors: true, depth: 10 }));
+
+    res.render('file_upload/multiparty', payload);
+  });
+});
+
+
 router.get('/:view', function(req, res, next) {
   res.render('file_upload/' + req.params.view, { title: req.params.view + ' | File Upload', data: {} });
 });
+
 
 
 
