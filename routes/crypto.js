978602c06c005c1530e8bc1954ed8ff0403c8868
tayutaedomo <tayutaedomo@gmail.com>
2019-11-30 16:07:05 +0900

    Add encrypt handling

--- routes/crypto.js
+++ routes/crypto.js
@@ -1,15 +1,50 @@
+const debug = require('debug')('node-js-web-sandbox:routes:crypto');
 const express = require('express');
 const router = express.Router();
 
+const crypto = require('crypto');
+
 
 router.post('/encrypt', (req, res) => {
-  // TODO
+  debug('encrypt', req.body);
+
+  // Refer: https://qiita.com/kou_pg_0131/items/174aefd8f894fea4d11a
+  const create32byte = (input) => {
+    return crypto.createHash('md5').update(input).digest('hex');
+  };
+
+  // Refer: https://qiita.com/Ishidall/items/bb0e0db86a2f56fb1022
+  const ENCRYPTION_KEY = create32byte(req.body.email); // 32Byte.
+  const BUFFER_KEY = 'RfHBdAR5RJHqp5wm'; // 16Byte.
+  const ENCRYPT_METHOD = 'aes-256-cbc';
+  const ENCODING = 'hex';
+
+  const getEncryptedString = (raw) => {
+    const iv = Buffer.from(BUFFER_KEY);
+    const cipher = crypto.createCipheriv(ENCRYPT_METHOD, Buffer.from(ENCRYPTION_KEY), iv);
+    let encrypted = cipher.update(raw);
+
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+    return encrypted.toString(ENCODING);
+  };
+
+
+  const local = {
+    title: 'Encrypt POST | Crypto',
+    data: {
+      result: getEncryptedString(req.body.text)
+    }
+  };
+
+  res.render('crypto/encrypt', local);
 });
 
 
 router.get('/:view', function(req, res, next) {
   const local = {
-    title: req.params.view + ' | Crypto', data: {}
+    title: req.params.view + ' | Crypto',
+    data: {}
   };
 
   res.render('crypto/' + req.params.view, local);
